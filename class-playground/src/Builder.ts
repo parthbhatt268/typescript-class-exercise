4e9678d3cb082b67441a774f833d91a3f2d4bde3
parthbhatt268 <parthbhatt268@gmail.com>
2025-12-06 23:55:08 +0000

    typo

--- class-playground/src/Builder.ts
+++ class-playground/src/Builder.ts
@@ -1,24 +1,21 @@
 export default class Pizza {
-    private size: "small" | "medium" | "large";
-    private cheese: boolean;
-    private pepper: boolean;
-    private salt: boolean;
+  private size: "small" | "medium" | "large";
+  private cheese: boolean;
+  private pepper: boolean;
+  private salt: boolean;
 
-    constructor(builder: PizzaBuilder) {
-        this.size = builder.size;
-        this.cheese = builder.cheese;
-        this.pepper = builder.pepper;
-        this.salt = builder.salt;
-    }
+  constructor(builder: PizzaBuilder) {
+    this.size = builder.size;
+    this.cheese = builder.cheese;
+    this.pepper = builder.pepper;
+    this.salt = builder.salt;
+  }
 
-    describe(): string {
-    const toppings = [
-      this.cheese && "cheese",
-      this.pepper && "pepper",
-      this.salt && "salt",
-    ]
-      .filter(Boolean)
-      .join(", ") || "plain";
+  describe(): string {
+    const toppings =
+      [this.cheese && "cheese", this.pepper && "pepper", this.salt && "salt"]
+        .filter(Boolean)
+        .join(", ") || "plain";
 
     return `A ${this.size} pizza with ${toppings}.`;
   }
@@ -53,9 +50,6 @@ export class PizzaBuilder {
     return new Pizza(this);
   }
 }
-const myPizza = new PizzaBuilder("medium")
-  .addCheese()
-  .addPepper()
-  .build();
+const myPizza = new PizzaBuilder("medium").addCheese().addPepper().build();
 
 console.log(myPizza.describe());
